4ea4b4cfdc98353a105d13329b97720da02d2846
Abdul Rehman Khan <khan456.tdm@gmail.com>
2025-02-08 00:00:22 +0500

    [🛠️]

--- components/Sections/Contact.jsx
+++ components/Sections/Contact.jsx
@@ -8,8 +8,8 @@ export default function Contact() {
       <SectionHeading text={"Get in touch"} />
       <SectionSubHeading text={"Let’s discuss how we can bring your vision to life"} />
 
-      <div className='w-[450px] mt-6 px-6 flex flex-col md:flex-row gap-2'>
-        <input type="text" className='bg-zinc-950 border-stone-900 text-stone-400 input input-bordered grow' placeholder='example@email.com' />
+      <div className='w-[450px] mt-6 px-6 flex flex-col md:flex-row gap-2 overflow-hidden'>
+        <input type="text" className='bg-zinc-950 border-stone-900 text-stone-400 input input-bordered lg:grow w-full' placeholder='example@email.com' />
         <input type="submit" value="Subscribe" className='btn' />
       </div>
     </section>
