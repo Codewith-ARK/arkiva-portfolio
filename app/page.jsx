7dfb18794c3612f02e9f1ec1cac179ba9722d332
Abdul Rehman Khan <khan456.tdm@gmail.com>
2025-02-16 13:35:04 +0500

    [🛠️]

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ export default function page() {
   return (
     <>
       <div>
-        <div className='relative overflow-hidden px-4 lg:px-32 flex flex-col gap-6 items-center justify-center h-[550px]'>
+        <div className='relative overflow-hidden px-4 lg:px-32 flex flex-col gap-4 items-center justify-center h-[550px]'>
           <div className='-z-0 absolute h-full w-full flex divide-x divide-zinc-900 justify-start items-center'>
             <div className='h-full w-[180px]'></div>
             <div className='h-full w-[180px]'></div>
@@ -25,12 +25,23 @@ export default function page() {
             <div className='h-full w-[180px]'></div>
             <div className='h-full w-[180px]'></div>
           </div>
+          <h2 className='md:w-[550px] text-xl font-medium z-10 text-center'>
+            <span className='mx-1 border-b-2 border-b-red-600 border-red-600'>
+              Web Design
+            </span>
+            and
+            <span className='mx-1 border-b-2 border-b-red-600 border-red-600'>
+              Web Development
+            </span>
+            Agency.
+          </h2>
           <h1 className='text-4xl lg:text-6xl text-center font-bold z-10'>
             Innovate<span className='text-red-600'>. </span>
             Elevate<span className='text-red-600'>. </span>
             Dominate<span className='text-red-600'>. </span>
           </h1>
-          <h2 className='text-xl font-medium z-10 lowercase text-center'>Crafting Digital Experiences That Drive Success.</h2>
+          <h2 className='md:w-[550px] text-lg font-medium z-10 text-center'>
+            Crafting Digital Experiences That Drive Success.</h2>
           <Link href='#contact' className='group z-10 btn rounded-full text-white border-transparent bg-red-600 hover:bg-red-800'>Let's Chat<LuChevronRight size={22} className='group-hover:translate-x-1 transition-all' /></Link>
         </div>
         <HeroGrid />
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import { BsCircle } from 'react-icons/bs'
 
 export default function Navbar() {
   return (
-    <nav className='sticky top-0 z-50 bg-black/70 backdrop-blur-md px-4 py-3 lg:px-32 lg:py-3 grid grid-cols-1 lg:grid-cols-2 items-center gap-3 '>
+    <nav className='sticky top-0 z-50 bg-transparent shadow-xl backdrop-blur-2xl px-4 py-3 lg:px-32 lg:py-3 grid grid-cols-1 lg:grid-cols-2 items-center gap-3 '>
       <Link href={"/"} className='flex justify-center lg:justify-start items-baseline gap-3'>
         <Image width={30} height={30} src={"/logo_light.png"} alt='brand logo' />
         <h1 className='text-3xl font-bold'>ARK<span className='text-red-600'>iva</span></h1>
