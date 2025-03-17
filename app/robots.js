0eada7f867f0b07e639ccd4d7983f7374fc0ffc2
Abdul Rehman Khan <khan456.tdm@gmail.com>
2025-03-17 21:00:31 +0500

    updated robots.txt

--- app/robots.js
+++ app/robots.js
@@ -2,9 +2,9 @@ export default function robots() {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/','/blog/*'],
       disallow: ['/private/', '/data/', '/app/data/'],
     },
-    sitemap: 'https://arkiva.vercel.app/',
+    sitemap: 'https://arkiva.vercel.app/sitemap.xml',
   }
 }
