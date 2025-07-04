9fa1ebf9bf8caf831092916f90c46736162ced82
Abdul Rehman Khan <khan456.tdm@gmail.com>
2025-07-04 18:50:25 +0500

    [+] added new projects

--- app/data/ProjectData.js
+++ app/data/ProjectData.js
@@ -8,14 +8,16 @@ const projects = [
   // },
   {
     title: "USMS Student Portal",
-    description: "A full-fledged students grievance portal for the students of University of Sufism and Modern Sciences, Bhitshah.",
+    description:
+      "A full-fledged students grievance portal for the students of University of Sufism and Modern Sciences, Bhitshah.",
     category: "Education",
     url: "https://usmsportal.vercel.app/",
-    imgUrl: "/usms_portal_mockup.png"
+    imgUrl: "/usms_portal_mockup.png",
   },
   {
     title: "Wedman",
-    description: "An open source wedding venue management web application to view, manage and book wedding venues",
+    description:
+      "An open source wedding venue management web application to view, manage and book wedding venues",
     category: "Event Management",
     url: "https://wedman.vercel.app/",
     imgUrl: "/wedman_mockup.png",
@@ -26,6 +28,20 @@ const projects = [
     category: "E-Commerce",
     url: "https://hackathon-template-08.vercel.app/",
     imgUrl: "/comforty.png",
+  },
+  {
+    title: "Video Editor Portfolio",
+    description: "A modern-stylized portfolio for a freelance video editor",
+    category: "Design & Development",
+    url: "https://hammas-portfolio.vercel.app/",
+    imgUrl: "/hammas-shah-portfolio.png",
+  },
+  {
+    title: "Campus Insights System",
+    description: "Smart Analytics for a Better Campus Experience.",
+    category: "Design & Development",
+    url: "https://campus-insights-system.vercel.app/",
+    imgUrl: "/campus-insights-system.png",
   },
 ];
 
